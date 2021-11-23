00f008d62f8d6b6ba19c9fe4bdcfe7a7619eb1c6
vismayatk002 <85096431+vismayatk002@users.noreply.github.com>
2021-11-23 22:24:41 +0530

    Update settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -102,12 +102,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbname',
-        'USER': 'user',
-        'PASSWORD': 'dsfdsfbdspfus',
-        'HOST': 'endpoint.google.com',
-        'PORT': '1234',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chatappdb',
+        'USER': 'admin',
+        'PASSWORD': 'vismayadeepak',
+        'HOST': 'database-3.cbsgr4lcbtyr.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 #sagar kadam
